f1df315406df36d4757deef5976b45e49b293675
silenthill99 <florian.graziani@sfr.fr>
2022-08-06 21:02:25 +0200

    Ajout d'une armure custom

--- src/main/java/fr/silenthill99/sonicmod/init/ModItems.java
+++ src/main/java/fr/silenthill99/sonicmod/init/ModItems.java
@@ -1,10 +1,14 @@
 package fr.silenthill99.sonicmod.init;
 
 import fr.silenthill99.sonicmod.Main;
+import fr.silenthill99.sonicmod.utils.CustomArmorMaterial;
 import fr.silenthill99.sonicmod.utils.CustomItemTiers;
 import fr.silenthill99.sonicmod.utils.ModItemGroups;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SwordItem;
+import net.minecraft.util.WeightedRandom;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,5 +24,8 @@ public class ModItems
     public static final RegistryObject<ForgeSpawnEggItem> AMY_ROSE_SPAWN_EGG = ITEMS.register("amy_rose_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.AMY, 0xFF0099, 0x404040, new Item.Properties().tab(ModItemGroups.sonic)));
 
     public static final RegistryObject<Item> PIKO_PIKO_HAMMER = ITEMS.register("piko_piko_hammer", () -> new SwordItem(CustomItemTiers.PIKO_PIKO_HAMMER, 4, -1.0f, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> TEST_HELMET = ITEMS.register("test_helmet", () -> new ArmorItem(CustomArmorMaterial.TEST, EquipmentSlotType.HEAD, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> TEST_CHESTPLATE = ITEMS.register("test_chestplate", () -> new ArmorItem(CustomArmorMaterial.TEST, EquipmentSlotType.CHEST, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_MATERIALS)));
+
 
 }
---
+++ src/main/java/fr/silenthill99/sonicmod/utils/CustomArmorMaterial.java
@@ -0,0 +1,71 @@
+package fr.silenthill99.sonicmod.utils;
+
+import fr.silenthill99.sonicmod.Main;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public enum CustomArmorMaterial implements IArmorMaterial {
+    TEST(Main.MODID + ":test", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, new LazyValue<>(() -> Ingredient.of(Items.NETHERITE_INGOT)))
+    ;
+    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    CustomArmorMaterial(String name, int durabilityMultiplier, int[] slotProtections, int enchantmentValue, SoundEvent sound, float toughness, float knockbackResistance, LazyValue<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.slotProtections = slotProtections;
+        this.enchantmentValue = enchantmentValue;
+        this.sound = sound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurabilityForSlot(EquipmentSlotType p_200896_1_) {
+        return HEALTH_PER_SLOT[p_200896_1_.getIndex()] * this.durabilityMultiplier;
+    }
+
+    public int getDefenseForSlot(EquipmentSlotType p_200902_1_) {
+        return this.slotProtections[p_200902_1_.getIndex()];
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
