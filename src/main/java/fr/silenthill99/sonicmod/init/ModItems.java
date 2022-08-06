1fb65692cb71866b38d8d35bb98a58556615d5db
silenthill99 <florian.graziani@sfr.fr>
2022-08-06 11:53:15 +0200

    Ajout du piko piko hammer d'Amy

--- src/main/java/fr/silenthill99/sonicmod/init/ModItems.java
+++ src/main/java/fr/silenthill99/sonicmod/init/ModItems.java
@@ -1,7 +1,10 @@
 package fr.silenthill99.sonicmod.init;
 
 import fr.silenthill99.sonicmod.Main;
+import fr.silenthill99.sonicmod.utils.CustomItemTiers;
 import fr.silenthill99.sonicmod.utils.ModItemGroups;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,6 +17,8 @@ public class ModItems
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
 
     public static final RegistryObject<ForgeSpawnEggItem> SONIC_THE_HEDGEHOG_SPAWN_EGG = ITEMS.register("sonic_the_hedgehog_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.SONIC, 0x0033CC, 0xcccc00, new Item.Properties().tab(ModItemGroups.sonic)));
-    public static final RegistryObject<ForgeSpawnEggItem> AMY_ROSE_SPAWN_EGG = ITEMS.register("amy_rose_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.AMY, 0x00ff99, 0x404040, new Item.Properties().tab(ModItemGroups.sonic)));
+    public static final RegistryObject<ForgeSpawnEggItem> AMY_ROSE_SPAWN_EGG = ITEMS.register("amy_rose_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.AMY, 0xFF0099, 0x404040, new Item.Properties().tab(ModItemGroups.sonic)));
+
+    public static final RegistryObject<Item> PIKO_PIKO_HAMMER = ITEMS.register("piko_piko_hammer", () -> new SwordItem(CustomItemTiers.PIKO_PIKO_HAMMER, 4, -1.0f, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_MATERIALS)));
 
 }
---
+++ src/main/java/fr/silenthill99/sonicmod/utils/CustomItemTiers.java
@@ -0,0 +1,58 @@
+package fr.silenthill99.sonicmod.utils;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+public enum CustomItemTiers implements IItemTier
+{
+    PIKO_PIKO_HAMMER(1, 500, 7.0f, 200, 20, new LazyValue<>(() -> Ingredient.of(Items.STICK)))
+    ;
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    CustomItemTiers(int level, int uses, float speed, float damage, int enchantmentValue, LazyValue<Ingredient> repairIngredient) {
+        this.level = level;
+        this.uses = uses;
+        this.speed = speed;
+        this.damage = damage;
+        this.enchantmentValue = enchantmentValue;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getUses() {
+        return uses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return damage;
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return repairIngredient.get();
+    }
+}
